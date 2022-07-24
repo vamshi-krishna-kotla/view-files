15b80236ae85027b0a2b15fd713124480b72d7e0
VamshiKrishnaKotla <vamshikrishna@elpaviation.com>
2022-07-24 16:24:41 +0530

    Moved 'returnFormattedChildrenTree' to a utils file

---
+++ src/client/utils/index.js
@@ -0,0 +1,36 @@
+
+/**
+ * function to format the JSON output coming from server
+ * 
+ * the formatted data holds Array, of the name, child content and
+ * Array of next directory in the depth
+ * 
+ * 
+ * @param {Object} parent JSON tree of specific directory
+ * @returns {Array} an array of the child nodes in the following format
+ * [
+ * 	<directory-name/filename>,
+ * 	<JSON for child nodes of current directory/null>,
+ * 	<Array format of data of child nodes>
+ * ]
+ * 
+ */
+export function returnFormattedChildrenTree(parent) {
+    let childrenNodes = [];
+    for (let child in parent) {
+        if (Object.hasOwnProperty.call(parent, child)) {
+            const element = parent[child];
+            childrenNodes.push([child, element]);
+        }
+    }
+    childrenNodes = childrenNodes.map(e => {
+        if (e[1] && e[1].isDirectory) {
+            e.push(returnFormattedChildrenTree(e[1].__child_nodes__));
+        }
+        else {
+            e.push(null);
+        }
+        return e;
+    });
+    return childrenNodes;
+}
