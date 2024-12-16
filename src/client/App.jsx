3a046c9b72d875ecd909a4f317cdfd90117a083e
Vamshi Krishna Kotla <kotlavamshi999@gmail.com>
2024-12-16 18:01:03 +0530

    updated app component for recursive rendering and cleared unused CSS

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,11 +1,9 @@
 /**
  * central React component to render the app
  */
-
-import { returnChildrenTree } from '../utils/index.js';
-
+import React, { useEffect, useState } from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
 import './App.scss';
-import './components/Components.scss';
 
 /**
  * React component that takes in props and renders the HTML tree
@@ -13,9 +11,48 @@ import './components/Components.scss';
  * @returns no return value
  */
 export default function App(props) {
+	let [children, setChildren] = useState([]);
 
+	useEffect(() => {
+		if (props.children && props.children.length) {
+			setChildren(props.children);
+		} else {
+			fetch(`/api${props.route}`)
+				.then(res => res.json())
+				.then(data => {
+					setChildren(data.children);
+				})
+				.catch(err => {
+					setChildren([]);
+					console.error(err);
+				});
+		}
+	}, []);
 	// return the HTML generated after filtering the JSON output from props
 	return (
-		returnChildrenTree(props.tree)
+		<>
+		<div className='content'>
+			{
+				children.map((child, index) => {
+					return (
+						<div key={'child_' + index} className="child">
+							<Link to={`${props.route}${child}/`}>{child}</Link>
+						</div>
+					);
+				})
+			}
+		</div>
+		<Switch>
+			{
+				children.map((child, index) => {
+					return (
+						<Route path={`${props.route}${child}/`} key={'route_' + index}>
+							<App route={`${props.route}${child}/`} children={[]} />
+						</Route>
+					);
+				})
+			}
+		</Switch>
+		</>
 	);
 }
